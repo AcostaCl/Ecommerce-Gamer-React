774df986612702c8f8c9f926f25f4c5a5cfa9f0c
Celina <idkmahee@gmail.com>
2025-05-17 14:07:34 -0300

    se hizo la estructura para el error404 y agregaron estilos para el boton

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -1,8 +1,20 @@
+import { Link } from "react-router";
+import "../../styles/Error.css";
+import imagenError from "../../img/error404.png";
 const Error404 = () => {
   return (
-    <div>
-      <h2>Aquí va el error 404 </h2>
-    </div>
+    <>
+      <div className="text-center bg-error d-flex flex-column align-items-center justify-content-center ">
+        <img
+          className="w-50 mb-2"
+          src={imagenError}
+          alt="Fondo error404 Mario Bros"
+        />
+        <Link className="btn btn-custom mb-4" to={"/"} variant="primary ">
+          Volver al inicio
+        </Link>
+      </div>
+    </>
   );
 };
 
