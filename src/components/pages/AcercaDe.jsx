68b047c972e10d3210a39cb24379ea836aec32be
Celina <idkmahee@gmail.com>
2025-05-18 13:13:52 -0300

    agregué estructura a la pagina de acerca de

--- src/components/pages/AcercaDe.jsx
+++ src/components/pages/AcercaDe.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import "../../styles/AcercaDe.css";
+import { Container } from "react-bootstrap";
 
 const AcercaDe = () => {
-  return <div>rsc</div>;
+  return (
+    <>
+      <section>
+        <Container className="text-center">
+          <h2 className="fw-bold">Sobre nosotros</h2>
+        </Container>
+      </section>
+    </>
+  );
 };
 
 export default AcercaDe;
