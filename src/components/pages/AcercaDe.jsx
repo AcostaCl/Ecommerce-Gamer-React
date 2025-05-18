713e3b38ba1d9ef97d7f8e33161fc3694789f3af
Celina <idkmahee@gmail.com>
2025-05-18 13:24:14 -0300

    agregué un subtitulo en el banner

--- src/components/pages/AcercaDe.jsx
+++ src/components/pages/AcercaDe.jsx
@@ -1,12 +1,22 @@
 import "../../styles/AcercaDe.css";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 const AcercaDe = () => {
   return (
     <>
       <section>
         <Container className="text-center">
-          <h2 className="fw-bold">Sobre nosotros</h2>
+          <article>
+            <h2 className="fw-bold">Sobre nosotros</h2>
+            <p className="text-muted">
+              {" "}
+              Vitbite es una tienda online dedicada a videojuegos retro,
+              consolas clásicas y nostalgia gamer
+            </p>
+          </article>
+          <article>
+            
+          </article>
         </Container>
       </section>
     </>
