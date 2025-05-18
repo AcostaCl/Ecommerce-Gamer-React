11e044af83fbbab5ea0b3f94f96e1cf9e2b93ff8
Celina <idkmahee@gmail.com>
2025-05-18 13:57:57 -0300

    quité las imagenes dentro de la seccion del banner y agregué etiquetas p para la seccion de informacion

--- src/components/pages/AcercaDe.jsx
+++ src/components/pages/AcercaDe.jsx
@@ -1,44 +1,41 @@
 import "../../styles/AcercaDe.css";
 import { Container, Row, Col } from "react-bootstrap";
-import imagen1 from "../../img/banner1.png";
-import imagen2 from "../../img/error404.png";
 
 const AcercaDe = () => {
   return (
     <>
-      <section className="bg-dark">
+      <section className="bg-dark py-5">
         <Container className="text-center">
           <article>
             <h2 className="fw-bold text-light">Sobre nosotros</h2>
             <p className="text-secondary">
-              {" "}
               Vitbite es una tienda online dedicada a videojuegos retro,
               consolas clásicas y nostalgia gamer
             </p>
-          </article>
-          <article>
-            <Row className="mt-4 justify-content-center">
-              <Col className="mb-3">
-                <img
-                  src={imagen1}
-                  alt=""
-                  width={100}
-                  className="img-fluid rounded"
-                />
-                <img
-                  src={imagen2}
-                  alt=""
-                  width={100}
-                  className="img-fluid rounded"
-                />
-              </Col>
-            </Row>
           </article>
         </Container>
       </section>
       <section>
         <Container className="py-5">
-          
+          <Row className="mb-5 align-items-center">
+            <Col className={6}>
+              <h3 className="fw-bold"></h3>{" "}
+              <p className="text-muted">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Excepturi error placeat veritatis porro quasi dolorem iste
+                nesciunt repellat dolor molestiae aspernatur, praesentium quis
+                veniam numquam a eos exercitationem quaerat officia.
+              </p>
+            </Col>
+            <Col md={6}>
+              <p className="text-muted">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Excepturi error placeat veritatis porro quasi dolorem iste
+                nesciunt repellat dolor molestiae aspernatur, praesentium quis
+                veniam numquam a eos exercitationem quaerat officia.
+              </p>
+            </Col>
+          </Row>
         </Container>
       </section>
     </>
