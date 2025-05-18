c3388a00b98b1d940d46bfd0a6eba2517846ac63
Celina <idkmahee@gmail.com>
2025-05-18 14:15:49 -0300

    agregué imagen y blockquote para poner datos

--- src/components/pages/AcercaDe.jsx
+++ src/components/pages/AcercaDe.jsx
@@ -1,5 +1,5 @@
 import "../../styles/AcercaDe.css";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
 const AcercaDe = () => {
   return (
@@ -21,19 +21,41 @@ const AcercaDe = () => {
             <Col className={6}>
               <h3 className="fw-bold"></h3>{" "}
               <p className="text-muted">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Excepturi error placeat veritatis porro quasi dolorem iste
-                nesciunt repellat dolor molestiae aspernatur, praesentium quis
-                veniam numquam a eos exercitationem quaerat officia.
+                Vitbite es una tienda online especializada en videojuegos retro,
+                creada con nostalgia, pasión y mucho cuidado por los clásicos.
+                Aquí encontrarás consolas, juegos y accesorios que marcaron
+                época, seleccionados para los verdaderos amantes del gaming de
+                antaño.
               </p>
             </Col>
             <Col md={6}>
               <p className="text-muted">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Excepturi error placeat veritatis porro quasi dolorem iste
-                nesciunt repellat dolor molestiae aspernatur, praesentium quis
-                veniam numquam a eos exercitationem quaerat officia.
+                Este proyecto está desarrollado y gestionado por una sola
+                persona. Cada parte del sitio, ha sido creado pensando en
+                ofrecer una experiencia auténtica y confiable. Vitbite no es
+                solo una tienda: es un rincón para revivir los mejores momentos
+                del pasado gamer.
               </p>
+            </Col>
+          </Row>
+          <Row className="align-items-center mb-5">
+            <Col md={6}>
+              <img
+                src="https://www.servnet.mx/hubfs/Backend-y-Frontend-%C2%BFQu%C3%A9-es-c%C3%B3mo-funcionan-en-la-programaci%C3%B3n.jpg"
+                alt="Equipo Front End"
+                width={400}
+                className="img-fluid"
+              />
+              <Card className="mt-3 shadow-sm">
+                <Card.Body>
+                  <blockquote className="blockquote mb-0">
+                    <p>Celina Acosta</p>
+                    <footer className="blockquote-footer">
+                      Desarrolladora web
+                    </footer>
+                  </blockquote>
+                </Card.Body>
+              </Card>
             </Col>
           </Row>
         </Container>
