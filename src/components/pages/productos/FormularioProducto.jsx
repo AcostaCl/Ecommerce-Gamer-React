7ec943f61da7312a5de5d6678a1c87baa8530221
Celina <idkmahee@gmail.com>
2025-05-20 10:33:59 -0300

    agregué un titulo en formulario producto

--- src/components/pages/productos/FormularioProducto.jsx
+++ src/components/pages/productos/FormularioProducto.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { Form, Button } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 const FormularioProducto = () => {
-  return <div></div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    setValue,
+  } = useForm();
+
+  return (
+    <section className="container">
+      <h2 className="display-4 mt-5 text-primary">Administrar producto</h2>
+      <hr />
+      
+    </section>
+  );
 };
 
 export default FormularioProducto;
