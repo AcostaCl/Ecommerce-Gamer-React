0de7b6365e595e0843b0756b69a58c4bcf7e95e0
Celina <idkmahee@gmail.com>
2025-05-20 10:56:42 -0300

    se modificaron los estilos de la pagina acerca de

--- src/components/pages/AcercaDe.jsx
+++ src/components/pages/AcercaDe.jsx
@@ -2,6 +2,7 @@ import "../../styles/AcercaDe.css";
 import { Container, Row, Col, Card } from "react-bootstrap";
 import { IoMdPerson, IoIosCodeWorking } from "react-icons/io";
 import { MdOutlineWork } from "react-icons/md";
+import imagenAcercaDe from "../../img/imagen-acercade.png";
 
 const AcercaDe = () => {
   return (
@@ -17,12 +18,12 @@ const AcercaDe = () => {
           </article>
         </Container>
       </section>
-      <section>
+      <section className="bg-purpura">
         <Container className="py-5">
           <Row className="mb-5 align-items-center">
             <Col className={6}>
               <h3 className="fw-bold"></h3>{" "}
-              <p className="text-muted">
+              <p className="text-light">
                 Vitbite es una tienda online especializada en videojuegos retro,
                 creada con nostalgia, pasión y mucho cuidado por los clásicos.
                 Aquí encontrarás consolas, juegos y accesorios que marcaron
@@ -31,7 +32,7 @@ const AcercaDe = () => {
               </p>
             </Col>
             <Col md={6}>
-              <p className="text-muted">
+              <p className="text-light">
                 Este proyecto está desarrollado y gestionado por una sola
                 persona. Cada parte del sitio, ha sido creado pensando en
                 ofrecer una experiencia auténtica y confiable. Vitbite no es
@@ -42,17 +43,12 @@ const AcercaDe = () => {
           </Row>
           <Row className="align-items-center mb-5">
             <Col md={6}>
-              <img
-                src="https://www.servnet.mx/hubfs/Backend-y-Frontend-%C2%BFQu%C3%A9-es-c%C3%B3mo-funcionan-en-la-programaci%C3%B3n.jpg"
-                alt="Equipo Front End"
-                width={400}
-                className="img-fluid"
-              />
-              <Card className="mt-3 shadow-sm">
+              <img src={imagenAcercaDe} width={200} className="img-fluid" />
+              <Card className="mt-3 shadow-sm card-acercade">
                 <Card.Body>
-                  <blockquote className="blockquote mb-0">
-                    <p>Celina Acosta</p>
-                    <footer className="blockquote-footer">
+                  <blockquote className="blockquote  mb-0">
+                    <p className="text-light">Celina Acosta</p>
+                    <footer className="blockquote-footer text-light">
                       Desarrolladora web
                     </footer>
                   </blockquote>
@@ -61,15 +57,15 @@ const AcercaDe = () => {
             </Col>
             <Col md={6}>
               <article className="px-2">
-                <h4 className="fw-bold mt-3">Acerca de mi</h4>
-                <p className="text-muted">
+                <h4 className="fw-bold text-light mt-3">Acerca de mi</h4>
+                <p className="text-light">
                   Disfruto trabajar de forma independiente, asumir nuevos retos
                   y construir experiencias digitales que conecten con las
                   personas. Además de programar, me interesa el diseño
                   funcional, la usabilidad y todo lo relacionado con el mundo
                   digital y visual.
                 </p>
-                <blockquote className="blockquote text-info border-start border-4 ps-3">
+                <blockquote className="blockquote text-purpura border-start border-4 ps-3">
                   <p>
                     “Vitbite nació de la nostalgia y crece con cada persona que
                     vuelve a jugar como antes.”
@@ -80,11 +76,11 @@ const AcercaDe = () => {
           </Row>
         </Container>
       </section>
-      <hr />
-      <section className="text-center py-5">
+      <section className="text-center py-5 bg-purpura">
+        <hr className="text-light my-4" />
         <article>
-          <h3 className="fw-bold">Unite al equipo</h3>
-          <p className="text-muted mb-5">
+          <h3 className="fw-bold text-light">Unite al equipo</h3>
+          <p className="text-light mb-5">
             Si te gusta emprender, construir desde cero y aportar ideas que
             marquen la diferencia, este puede ser tu lugar.
           </p>
@@ -92,23 +88,23 @@ const AcercaDe = () => {
         <article>
           <Row>
             <Col md={4} className="mb-4">
-              <IoMdPerson size={40} className="text-info mb-3" />
-              <h5 className="fw-bold">Horario</h5>
-              <p className="text-muted">
+              <IoMdPerson size={40} className="text-purpura mb-3" />
+              <h5 className="fw-bold text-light">Horario</h5>
+              <p className="text-light">
                 Flexibilidad horaria y trabajo remoto
               </p>
             </Col>
             <Col md={4} className="mb-4">
-              <IoIosCodeWorking size={40} className="text-info mb-3" />
-              <h5 className="fw-bold">Creatividad</h5>
-              <p className="text-muted">
+              <IoIosCodeWorking size={40} className="text-purpura mb-3" />
+              <h5 className="fw-bold text-light">Creatividad</h5>
+              <p className="text-light">
                 Libertad creativa para proponer y construir
               </p>
             </Col>
             <Col md={4} className="mb-4">
-              <MdOutlineWork size={40} className="text-info mb-3" />
-              <h5 className="fw-bold">Personal</h5>
-              <p className="text-muted">
+              <MdOutlineWork size={40} className="text-purpura mb-3" />
+              <h5 className="fw-bold text-light">Personal</h5>
+              <p className="text-light">
                 Aprendizaje constante y ambiente sin jerarquías
               </p>
             </Col>
