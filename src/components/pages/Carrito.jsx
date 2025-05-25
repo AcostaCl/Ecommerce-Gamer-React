d5295c76eaf7927344ef35eb3cebc3709eef3548
Celina <idkmahee@gmail.com>
2025-05-24 23:39:18 -0300

    agregue estilos al carrito

--- src/components/pages/Carrito.jsx
+++ src/components/pages/Carrito.jsx
@@ -3,20 +3,21 @@ import "../../styles/Carrito.css";
 
 const Carrito = () => {
   return (
-    <section className="fondo-carrito">
-      <div>
-        <h2 className="text-center py-4">Tu carrito</h2>
+    <section className="fondo-carrito p-3">
+      <div className="banner-carrito">
+        <h2 className="text-center py-4 text-carrito">Tu carrito</h2>
       </div>
       <Container className="mt-5">
-        <h2 className="text-light mb-4">Carrito de Compras</h2>
+        <h2 className="mb-4 text-info">Carrito de Compras</h2>
+        <hr className="text-info" />
         <p className="text-light">El carrito está vacío.</p>
-        <Table striped bordered hover variant="dark" responsive>
+        <Table responsive className="tabla-carrito">
           <thead>
             <tr>
-              <th>Imagen</th>
-              <th>Nombre</th>
-              <th>Precio</th>
-              <th>Acciones</th>
+              <th className="text-info">Imagen</th>
+              <th className="text-info">Nombre</th>
+              <th className="text-info">Precio</th>
+              <th className="text-info"></th>
             </tr>
           </thead>
           <tbody>
@@ -25,17 +26,25 @@ const Carrito = () => {
                 <img
                   src="https://imag.bonviveur.com/cupcakes.jpg"
                   alt="cupcake"
+                  className="imagen-carrito"
                 />
               </td>
-              <td></td>
-              <td>$</td>
+              <td>Nombre del producto</td>
+              <td>$99</td>
               <td>
-                <Button variant="danger">Eliminar</Button>
+                <div>
+                  <Button className="boton-carrito" size="sm">
+                    Eliminar del carrito
+                  </Button>
+                </div>
               </td>
             </tr>
           </tbody>
         </Table>
-        <h4 className="text-light text-end">Total: </h4>)
+        <div className="d-flex justify-content-between align-items-center">
+          <h3 className="text-light">Total: </h3>
+          <Button size="sm">Proceder al pago</Button>
+        </div>
       </Container>
     </section>
   );
